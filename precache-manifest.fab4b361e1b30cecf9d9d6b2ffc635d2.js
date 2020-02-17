cfaf04dd0ae8047eb9aa9ad5936fd933e3d9624f
Sandeep Pyati <sapyati.smtm@gmail.com>
2020-02-17 16:12:18 +0530

    Updates

---
+++ precache-manifest.fab4b361e1b30cecf9d9d6b2ffc635d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ea813ba7f9221200cd7af26d18c268",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "d18392cd1a36290233ce",
+    "url": "/react-todo-app/static/css/main.e9521184.chunk.css"
+  },
+  {
+    "revision": "dd5da97469e3dd0b2919",
+    "url": "/react-todo-app/static/js/2.42cb23ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todo-app/static/js/2.42cb23ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18392cd1a36290233ce",
+    "url": "/react-todo-app/static/js/main.65b24214.chunk.js"
+  },
+  {
+    "revision": "a916b4df2d3ac2ce6feb",
+    "url": "/react-todo-app/static/js/runtime-main.70d5ea2a.js"
+  }
+]);
